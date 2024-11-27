bfcd24a2c7768642bfc1b8de09dfc3ec9990a37f
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-27 21:54:23 +0500

    pre and post

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ var BODMAS =1*3+10 ;
 console.log(BODMAS);
 
 // ans is 13
+
+
+var BODMAS1 =(3*3)+(10*100) ;
+console.log(BODMAS1);
+
+// ans is 1009
+
+
+// pre and post 
+var number =0 ;
+++number;
+++number;
+++number;
+++number;
+++number;
+++number;
+++number;
+console.log(number);
